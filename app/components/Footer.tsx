20d2f77069ebed905b537ca06fd9d6f6e80d68b7
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2025-01-10 22:44:33 +0530

    changes done

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,11 +7,11 @@ const Footer: React.FC = () => {
     <footer className="w-full py-4 px-6 flex justify-between items-center border-t border-gray-200 mb-1 font-['helveticanowtext-black-demo] font-extrabold">
       <div className="text-base lg:text-4xl ">
         The
-        <span className="inline-block mx-1 w-1 h-1 lg:w-3 lg:h-3 bg-black rounded-full font-['Uniform_Rounded_Condensed_Black]"></span>
+        <span className="inline-block w-1 h-1 lg:w-3 lg:h-3 bg-black rounded-full font-['Uniform_Rounded_Condensed_Black]"></span>
         Now
-        <span className="inline-block mx-1 w-1 h-1 lg:w-3 lg:h-3 bg-black rounded-full font-['Uniform_Rounded_Condensed_Black]"></span>
+        <span className="inline-block  w-1 h-1 lg:w-3 lg:h-3 bg-black rounded-full font-['Uniform_Rounded_Condensed_Black]"></span>
         Company
-        <span className="inline-block mx-1 w-1 h-1 lg:w-3 lg:h-3 bg-black rounded-full font-['Uniform_Rounded_Condensed_Black]"></span>
+        <span className="inline-block  w-1 h-1 lg:w-3 lg:h-3 bg-black rounded-full font-['Uniform_Rounded_Condensed_Black]"></span>
       </div>
 
       <div className="text-xs lg:text-base text-gray-700 font-semibold font-['helveticanowtext-black-demo]">
--- app/components/NowValuationTool.tsx
+++ app/components/NowValuationTool.tsx
@@ -6,11 +6,11 @@ const NowValuationTool = () => {
       <div className="w-full py-4 px-6 flex justify-between items-center border-t border-gray-200 mb-1 bg-black">
         <div className="text-xl lg:text-5xl font-extrabold text-white font-['Helvetica']">
           The
-          <span className="inline-block mx-1 w-2 h-2 lg:w-3 lg:h-3 bg-white rounded-full"></span>
+          <span className="inline-block  w-2 h-2 lg:w-3 lg:h-3 bg-white rounded-full"></span>
           Now
-          <span className="inline-block mx-1 w-2 h-2 lg:w-3 lg:h-3 bg-white rounded-full"></span>
+          <span className="inline-block  w-2 h-2 lg:w-3 lg:h-3 bg-white rounded-full"></span>
           Valuation
-          <span className="inline-block mx-1 w-2 h-2 lg:w-3 lg:h-3 bg-white rounded-full"></span>
+          <span className="inline-block  w-2 h-2 lg:w-3 lg:h-3 bg-white rounded-full"></span>
           Tool
         </div>
       </div>
