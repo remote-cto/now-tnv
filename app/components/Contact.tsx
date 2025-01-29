242b389458b8d1eacfd90cec92acdae14f1778d3
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2025-01-29 20:32:16 +0530

    Text Changes done

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -2,27 +2,32 @@ import React from "react";
 
 const Contact: React.FC = () => {
   return (
-    <div className="bg-black flex flex-col items-center justify-center text-white py-12 px-4 mb-4 font-['Helvetica'] ">
+    <div className="bg-black flex flex-col items-center justify-center text-white py-12 px-4 mb-4  font-['helveticanowtext-black-demo']">
       <div className="max-w-3xl w-full text-center space-y-8">
-        <h1 className=" text-3xl lg:text-5xl font-extrabold mb-6 font-['Helvetica'] ">
+        <h1 className=" text-3xl lg:text-6xl font-extrabold mb-6 font-['Helvetica'] ">
           Need Help?
         </h1>
 
         <div className="space-y-4">
-          <p className="text-2xl font-['helveticanowtext-black-demo]">
+          <p className=" text-lg lg:text-2xl leading-tight font-['helveticanowtext-black-demo']">
             We understand that buying or selling a digital business isn't easy.
           </p>
-          <p className="text-2xl font-['helveticanowtext-black-demo]">
+          <p className="text-xl lg:text-2xl font-['helveticanowtext-black-demo]">
             If you have any questions or require assistance, feel free to
             contact us anytime.
           </p>
         </div>
 
         <div className="space-y-4 mt-12">
-          <p className="text-xl font-['helveticanowtext-black-demo]">
-            email: hello@nowvaluation.com
+          <p className="text-lg lg:text-2xl font-['helveticanowtext-black-demo]">
+            email : {" "}
+            <a 
+              href="mailto:hello@nowvaluation.com"
+              className="text-white hover:text-gray-300 underline"
+            >
+              hello@nowvaluation.com
+            </a>
           </p>
-          
         </div>
       </div>
     </div>
--- app/components/WorkingValuationTool.tsx
+++ app/components/WorkingValuationTool.tsx
@@ -1,11 +1,10 @@
-
 import React from "react";
 
 const WorkingValuationTool = () => {
   return (
     <div className="min-h-[400px] w-full bg-black flex flex-col items-center justify-center px-6 font-['helveticanowtext-black-demo'] ">
-      <div className="max-w-3xl">
-        <p className="text-white text-lg lg:text-2xl font-semibold mb-6 leading-tight font-['helveticanowtext-black-demo'] mt-6 lg:mt-1">
+      <div className="max-w-3xl space-y-8">
+        <p className="text-white text-lg lg:text-2xl font-semibold leading-tight font-['helveticanowtext-black-demo'] mt-6 lg:mt-1">
           Every business has a story, and understanding its value is a key part
           of that journey<span className="font-sans font-bold">.</span> Valuation<span className="text-red-600 font-sans font-bold">.</span>Pro offers a clear, data-driven way to
           understand your business's worth. With a powerful algorithm at its
@@ -13,12 +12,11 @@ const WorkingValuationTool = () => {
           moments.
         </p>
 
-        <p className="text-white text-lg lg:text-2xl font-semibold  leading-tight font-['helveticanowtext-black-demo']">
+        <p className="text-white text-lg lg:text-2xl font-semibold leading-tight font-['helveticanowtext-black-demo']">
           It's a straightforward, no-frills approach to help you see your
           business from a fresh perspective.
         </p>
       </div>
-      {/* <div className="w-full h-px bg-white mt-16"></div> */}
     </div>
   );
 };
