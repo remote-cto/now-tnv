07a2a9f69afdcb2fa19825c2aab4828e1944af07
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2025-01-30 13:23:23 +0530

    minor chnages done

--- app/components/WorkingValuationTool.tsx
+++ app/components/WorkingValuationTool.tsx
@@ -4,7 +4,7 @@ const WorkingValuationTool = () => {
   return (
     <div className="min-h-[400px] w-full bg-black flex flex-col items-center justify-center px-6 font-['helveticanowtext-black-demo'] ">
       <div className="max-w-3xl space-y-8">
-        <p className="text-white text-lg lg:text-2xl font-semibold leading-tight font-['helveticanowtext-black-demo'] mt-6 lg:mt-1">
+        <p className="text-white text-lg lg:text-2xl font-semibold leading-tight font-['helveticanowtext-black-demo'] ">
           Every business has a story, and understanding its value is a key part
           of that journey<span className="font-sans font-bold">.</span> Valuation<span className="text-red-600 font-sans font-bold">.</span>Pro offers a clear, data-driven way to
           understand your business's worth. With a powerful algorithm at its
